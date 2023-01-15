c848c7fbeaf13926ec223c7c718a61b5782d593d
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-01-15 21:37:48 +0200

    updated tests

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -4,19 +4,72 @@ from test import base_test
 
 def test_bear():
     tree = base_test('data/bear.csv', 'Class', 'out/bear-tree.json')
-    ref_tree = DecisionTree().load_tree_json(r'data\correct_trees\bear-tree.json')
-    assert tree == ref_tree
+    assert tree.tree_to_dict() == {
+        "Size": {
+            "Big": "Dangerous",
+            "Middle": {
+                "Color": {
+                    "Black": "No",
+                    "Brown": {
+                        "Hair": {
+                            "Curls": "No",
+                            "Straight": "Dangerous"
+                        }
+                    }
+                }
+            },
+            "Small": {
+                "Color": {
+                    "Black": "No",
+                    "Brown": {
+                        "Hair": {
+                            "Curls": "Dangerous",
+                            "Straight": "No"
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
 def test_tennis():
     tree = base_test('data/tennis.csv', 'Play', 'out/tennis-tree.json')
-    ref_tree = DecisionTree().load_tree_json(
-        r'data\correct_trees\tennis-tree.json')
-    assert tree == ref_tree
+    assert tree.tree_to_dict() == {
+        "Outlook": {
+            "Overcast": "Yes",
+            "Rainy": {
+                "Windy": {
+                    "Strong": "No",
+                    "Weak": "Yes"
+                }
+            },
+            "Sunny": {
+                "Humidity": {
+                    "High": "No",
+                    "Normal": "Yes"
+                }
+            }
+        }
+    }
 
 
 def test_computer():
     tree = base_test('data/computer.csv', 'Buy', 'out/computer-tree.json')
-    ref_tree = DecisionTree().load_tree_json(
-        r'data\correct_trees\computer-tree.json')
-    assert tree == ref_tree
+    assert tree.tree_to_dict() == {
+        "Age": {
+            "Middle": "Yes",
+            "Old": {
+                "Credit": {
+                    "Good": "No",
+                    "Ok": "Yes"
+                }
+            },
+            "Young": {
+                "Student": {
+                    "No": "No",
+                    "Yes": "Yes"
+                }
+            }
+        }
+    }
